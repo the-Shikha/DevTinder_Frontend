e795135ed0efd06b8854ff50bbc5448645a630a8
shikha <shikhakumari152019@gmail.com>
2025-05-29 17:06:05 +0530

    Add routing and new components for user authentication and profile management

---
+++ src/components/Body.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from 'react-router-dom'
+import NavBar from './NavBar'
+import Footer from './Footer'
+
+
+const Body = () => {
+  return (
+    <div>
+      <NavBar/>
+      <Outlet/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Body
+
